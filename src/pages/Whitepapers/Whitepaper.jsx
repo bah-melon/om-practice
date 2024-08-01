f6ae10cd9dadf58db00028cc31e40e0eff89c5ab
artyshab <156674256+artyshab@users.noreply.github.com>
2024-08-01 10:37:31 +0200

    footer-work

--- src/pages/Whitepapers/Whitepaper.jsx
+++ src/pages/Whitepapers/Whitepaper.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
+import Prefooter from "../../components/prefooter/Prefooter";
 export default function Whitepaper(){
     return (
         <div>
-            <h1>Whitepaper</h1>
+            <Prefooter></Prefooter>
             <Footer></Footer>
         </div>
     )
