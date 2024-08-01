bb99ee97c24b2497e5b3019fa5eb809c1b7b688f
martytrajkoski <martytrajkoski@gmail.com>
2024-08-01 13:53:28 +0200

    navbar mobile version fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,6 @@ export default function Navbar(){
         <div className='navbar'>
             <NavbarLogo/>
             <Navigation/>
-            {/* <NavigationMenu/> */}
         </div>
     )
 }
